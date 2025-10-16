259db60ad0cc499b549bb4d803e46544e288c7c4
jaidmaner <mohmadjaid.maner23@pcu.edu.in>
2025-10-16 10:42:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
           <a href="https://www.linkedin.com/in/mohmadjaid-maner-201715289?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="text-white me-3">
             <i className="bi bi-linkedin"></i>
           </a>
-          <a href="https://github.com/jaidmaner/Website.git" className="text-white me-3">
+          <a href="https://github.com/jaidmaner" className="text-white me-3">
             <i className="bi bi-github"></i>
           </a>
          
